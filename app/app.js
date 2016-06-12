e9092b741b1c92ef4bcca2f668409925a02b9f6b
sergeyradkov <reriic@ya.ru>
2016-06-12 17:49:15 -0600

    123

--- app/app.js
+++ app/app.js
@@ -4,12 +4,28 @@
 angular.module('comics').config(function($stateProvider){
             
         $stateProvider  
-                .state('comicsbooks', {
-                    url: '',
+                .state('books', {
+                    url: '/books',
                     template: '<comicsbooks-component></comicsbooks-component>'
                 })
-                .state('comicsbook', {
-                    url: '/:book',
+                .state('book', {
+                    url: '/book',
+                    template: '<comicsbook-component></comicsbook-component>'
+                })
+                .state('movies', {
+                    url: '/movies',
+                    template: '<comicsbooks-component></comicsbooks-component>'
+                })
+                .state('movies', {
+                    url: '/movie',
+                    template: '<comicsbook-component></comicsbook-component>'
+                })
+                .state('heroes', {
+                    url: '/heroes',
+                    template: '<comicsbooks-component></comicsbooks-component>'
+                })
+                .state('hero', {
+                    url: '/hero',
                     template: '<comicsbook-component></comicsbook-component>'
                 })
 });
