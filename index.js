cd3874777c4cc236b6a5426859d4adb842753237
sergeyradkov <radkovsn@gmail.com>
2016-07-30 17:09:25 -0600

    12

--- index.js
+++ index.js
@@ -97,3 +97,11 @@ function getHero (hero, cb) {
 server.listen(port, function(){
     console.log("Server is running on port " + port);
 })
+
+server.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'YOUR_VERIFY_TOKEN') {
+      res.send(req.query['hub.challenge']);
+    } else {
+      res.send('Error, wrong validation token');    
+    }
+  });
